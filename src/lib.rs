0532a3db37019301231057876d820ad90c8eae76
STEFAN S <stefan@work.dd>
2026-09-23 14:09:53 +0200

    test: prüfe korrekte speicherung von real- und imaginär-teil in struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    println!("dummyfunktion aus lib");
-    left + right
+//#################################################################
+//#################-----eigeneSTRUKTUREN-----######################
+//#################################################################
+pub struct Complex {
+    pub real: f64,
+    pub imag: f64,
 }
 
+//#################################################################
+//###################-----eigeneMETHODEN-----######################
+//#################################################################
+impl Complex {
+    pub fn neu(real_teil: f64, imaginaer_teil: f64) -> Self {
+        unimplemented!()
+    }
+}
+
+//#################################################################
+//####################-----TDDbereich-----#########################
+//#################################################################
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    //##############-----komplexeARITHMETIK-----#######################
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_neue_complexe_struktuer_erstellen() {
+        let cpx = Complex::neu(1.1, -2.2);
+        assert_eq!(cpx.real, 1.1, "REALteil nicht korrekt gesetzt");
+        assert_eq!(cpx.imag, -2.2, "IMAGINÄRrteil nicht korrekt gesetzt");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use mandelbrot_menge_tdd::*;
+//use mandelbrot_menge_tdd::*;
 
 fn main() {
     println!("MANDELBROT");
-    add(5, 5);
 }
